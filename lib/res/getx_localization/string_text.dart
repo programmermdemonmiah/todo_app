2b0eeaa6c981ed342c30ec16795fa294f3776f3e
Md. Emon Miah <md.emonmiahisthebest@gmail.com>
2024-11-23 11:09:19 +0600

    update

--- lib/res/getx_localization/string_text.dart
+++ lib/res/getx_localization/string_text.dart
@@ -3,4 +3,6 @@ import 'package:get/get.dart';
 class S {
   static String get home => 'home'.tr;
   static String get signIn => 'signIn'.tr;
+  static String get enterYourPhoneNumber => 'enterYourPhoneNumber'.tr;
+  static String get phone => 'phone'.tr;
 }
